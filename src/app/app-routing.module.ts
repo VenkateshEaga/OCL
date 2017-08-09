012742da1d0856c840b8e4e430bd4efc3a65c7c3
vannem-nisum-com <vannem@nisum.com>
2017-08-09 14:07:28 +0530

    Resolving conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { TeamDetailComponent } from './cricket/teams/team-detail/team-detail.component';
 import { TeamsComponent } from './cricket/teams/teams.component';
 import { AuctionComponent } from './cricket/auction/auction.component';
-import { TeamsComponent } from './cricket/teams/teams.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { HomeComponent } from './home/home.component';
@@ -13,13 +12,7 @@ const routes: Routes = [
     { path: 'signin', component: SigninComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'auction', component: AuctionComponent },
-<<<<<<< HEAD
-    { path: 'teams', component: TeamsComponent ,children:[
-        { path:':id',component:TeamDetailComponent}
-    ] }
-=======
     { path: 'teams', component: TeamsComponent }
->>>>>>> Dataintegration
 ]
 
 @NgModule({
