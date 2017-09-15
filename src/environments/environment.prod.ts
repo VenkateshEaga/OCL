967e89a00ed996f8263573f6157b31adbc6f04b1
vannem-nisum-com <vannem@nisum.com>
2017-09-15 10:27:48 +0530

    config changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
   teamBudget: 200000,
-  fullTeamCount: 17,
+  fullTeamCount: 16,
   applyDEPConstraint: true,
   departmentConstraints: {
     'EA' : 2,
-    'Ops' : 3,
+    'Ops' : 2,
     'ET-Macys': 5,
     'ET-Engg': 3,
     'ET-GAP': 2,
-    'ET-Others': 3  
+    'ET-Others': 2  
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,14 @@
 export const environment = {
   production: false,
   teamBudget: 200000,
-  fullTeamCount: 17,
+  fullTeamCount: 16,
   applyDEPConstraint: true,
   departmentConstraints: {
     'EA' : 2,
-    'Ops' : 3,
+    'Ops' : 2,
     'ET-Macys': 5,
     'ET-Engg': 3,
     'ET-GAP': 2,
-    'ET-Others': 3  
+    'ET-Others': 2  
   }
 };
